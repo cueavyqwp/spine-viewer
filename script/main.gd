d4447e590fba1fbb943694fb2d6b790370017975
cueavyqwp <127200270+cueavyqwp@users.noreply.github.com>
2025-02-06 02:54:04 +0800

    修复缩放BUG与全屏BUG

--- script/main.gd
+++ script/main.gd
@@ -14,10 +14,10 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("Fullscreen"):
-		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
-			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-		else:
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
 func _on_button_load_pressed() -> void:
 	var path_skel : String = $UI/info/label_file.path_skel
--- script/spine_sprite.gd
+++ script/spine_sprite.gd
@@ -13,14 +13,20 @@ func _input(_event: InputEvent) -> void:
 	elif Input.is_action_just_released("MouseRight"):
 		last_mouse_position = Vector2.ZERO
 
-	if Input.is_action_just_pressed("Ctrl"):
+	if Input.is_action_pressed("Ctrl"):
 		zoom_speed = 0.1
+	elif Input.is_action_pressed("Shift"):
+		zoom_speed = 0.001
 	else:
-		zoom_speed = 0.025
+		zoom_speed = 0.05
 
 	if Input.is_action_just_released("WheelUp"):
 		zoom_factor += zoom_speed
 	elif Input.is_action_just_released("WheelDown"):
 		zoom_factor -= zoom_speed
+
+	zoom_factor = clamp( zoom_factor , 0.01 , 8 )
+
+	print(zoom_speed," ",zoom_factor)
 
 	scale = Vector2(zoom_factor, zoom_factor)
